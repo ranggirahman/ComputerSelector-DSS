9772745221eadb71ad4e38667fe8d67c7faf6d5e
ranggirahman <ranggirahman@gmail.com>
2018-01-26 11:54:12 +0700

    add-button

--- index.php
+++ index.php
@@ -57,35 +57,35 @@
 		<div class="container">
 	      	<div class="row">
 	        	<div class="col-md-4">
-	        		<div class="card" style="width: 18rem;">					  	
+	        		<div class="card">					  	
 						<div class="card-body">
 							<img src="img/processor.png" style="width: 100px">
 							<hr>
 							<h5 class="card-title">Processor</h5>
 						   	<p class="card-text"> Fungsi utama dari CPU adalah melakukan operasi aritmetika dan logika terhadap data yang diambil dari memori atau dari informasi yang dimasukkan</p>
-						   	<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#processormodal">View Processor List</a>
+						   	<div class="btn btn-primary" data-toggle="modal" data-target="#processormodal">View Processor List</div>
 						 </div>
 					</div>
 	        	</div>
 	        	<div class="col-md-4">
-	        		<div class="card" style="width: 18rem;">					  	
+	        		<div class="card">					  	
 						<div class="card-body">
 							<img src="img/vga.png" style="width: 142px">
 							<hr>
 							<h5 class="card-title">Video Graphic Array</h5>
 						   	<p class="card-text">Kartu VGA berguna untuk menerjemahkan keluaran komputer ke monitor. Untuk menggambar / design graphic ataupun untuk bermain game</p>
-						   	<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#vgamodal">View VGA List</a>
+						   	<div class="btn btn-primary" data-toggle="modal" data-target="#vgamodal">View VGA List</div>
 						 </div>
 					</div>
 	        	</div>
 	        	<div class="col-md-4">
-	        		<div class="card" style="width: 18rem;">					  	
+	        		<div class="card">					  	
 						<div class="card-body">
 							<img src="img/ssd.png" style="width: 100px">
 							<hr>
 							<h5 class="card-title">Solid State Disk</h5>
-						   	<p class="card-text">Sebuah komponen perangkat keras yang menyimpan data sekunder. Menggunakan nonvolatile memory sebagai media</p>
-						   	<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#ssdmodal">View SSD List</a>
+						   	<p class="card-text">Komponen perangkat keras yang menyimpan data sekunder. Menggunakan menggunakan nonvolatile memory tidak seperti hardisk konvensional</p>
+						   	<div href="" class="btn btn-primary" data-toggle="modal" data-target="#ssdmodal">View SSD List</div>
 						 </div>
 					</div>
 	        	</div>
@@ -108,7 +108,13 @@
 		  	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 		    	<div class="modal-content">
 		      		<div class="modal-header">
-		        		<img src="img/processor.png" style="max-width: 3%; and height: auto"><h5 class="modal-title" id="exampleModalLongTitle">&nbsp; Processor List</h5>
+
+		      			<?php 
+		      				$cpu = mysqli_query($koneksi,"select *from cpu");
+		      				$rcpu = mysqli_num_rows($cpu);
+						?>		
+
+		        		<img src="img/processor.png" style="max-width: 3%; and height: auto"><h5 class="modal-title" id="exampleModalLongTitle">&nbsp; Processor List (<?php echo $rcpu; ?> Product)</h5>
 		        		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 		          			<span aria-hidden="true">&times;</span>
 		        		</button>
@@ -118,7 +124,7 @@
 			        		<thead class="thead-light">
 				      			<tr>
 				      				<th>No</th>
-				      				<th width="30%">CPU Name</th>
+				      				<th width="20%">CPU Name</th>
 				      				<th>Performance</th>
 				      				<th>Single-Core Performance</th>
 				      				<th>Integrated Graphics</th>
@@ -126,10 +132,10 @@
 				      				<th>Performance Per Watt</th>
 				      				<th>Value</th>
 				      				<th width="10%">Total Score</th>
+				      				<th></th>
 				      			</tr>
 			      			</thead>
-			      			<?php
-			      				$cpu = mysqli_query($koneksi,"select *from cpu");			      				
+			      			<?php			      							      				
 								$i = 0;
 
 			      				if($cpu == TRUE){
@@ -154,20 +160,50 @@
 						      				<td><?php echo $ppw ?></td>
 						      				<td><?php echo $value ?></td>
 						      				<td><b><?php echo $cpuscore ?></b></td>
+						      				<td></td>
 						      			</tr>
 
 						      			<?php
 									}
 								}
 			      			?>
-						</table>
 			      	</div>
-			      	<div class="modal-footer">
-			        	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			        	<button type="button" class="btn btn-primary">Save changes</button>
+			      	<div class="modal-footer">	
+			      			<form action="" method="POST">		      						        	
+				        		<tr>
+				      				<td></td>
+				      				<td><input class="form-control" type="text" name="cpuname" placeholder="Processor Name" maxlength="100" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="performance" placeholder="Performance" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="single" placeholder="Single-Core Performance" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="intg" placeholder="Integrated Graphics" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="intgocl" placeholder="Integrated Graphics (OpenCL)" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="ppw" placeholder="Performance Per Watt" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="value" placeholder="Value" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="cpuscore" placeholder="Score" maxlength="5" required></td>
+				      				<td><input class="btn btn-success" type="submit" name="cpuadd" value="Add"></td>
+				      			</tr>
+				      		</form>
+						</table>
 			      	</div>
 		    	</div>
 		  	</div>
+		  	<?php
+			  	if(isset($_POST['cpuadd'])){
+
+				    $cpuname = $_POST['cpuname'];
+				    $performance = $_POST['performance'];
+				    $single = $_POST['single'];
+				    $intg = $_POST['intg'];
+				    $intgocl = $_POST['intgocl'];
+				    $ppw = $_POST['ppw'];
+				    $value = $_POST['value'];
+				    $cpuscore = $_POST['cpuscore'];
+
+				    $cpuadd = mysqli_query($koneksi,"insert into cpu(cpuname,performance,single,intg,intgocl,ppw,value) values ('$cpuname','$performance','$single','$intg','$intgocl','$ppw','$value','$cpuscore')");
+
+				    echo "<meta http-equiv='refresh' content='0'>";						   
+			  	}
+			?>
 		</div>  
 
 		<!-- VGA Modal -->
@@ -175,7 +211,13 @@
 		  	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 		    	<div class="modal-content">
 		      		<div class="modal-header">
-		        		<img src="img/vga.png" style="max-width: 4%; and height: auto"><h5 class="modal-title" id="exampleModalLongTitle">&nbsp; Video Graphic Array List</h5>
+
+		      			<?php 
+		      				$vga = mysqli_query($koneksi,"select *from vga");
+		      				$rvga = mysqli_num_rows($vga);
+						?>		      			
+
+		        		<img src="img/vga.png" style="max-width: 4%; and height: auto"><h5 class="modal-title" id="exampleModalLongTitle">&nbsp; Video Graphic Array List (<?php echo $rvga; ?> Product)</h5>
 		        		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 		          			<span aria-hidden="true">&times;</span>
 		        		</button>
@@ -185,7 +227,7 @@
 			        		<thead class="thead-light">
 				      			<tr>
 				      				<th>No</th>
-				      				<th width="30%">VGA Name</th>
+				      				<th width="20%">VGA Name</th>
 				      				<th>Gaming</th>
 				      				<th>Graphics</th>
 				      				<th>Computing</th>
@@ -193,10 +235,10 @@
 				      				<th>Value</th>
 				      				<th>Noise and Power</th>
 				      				<th width="10%">Total Score</th>
+				      				<th></th>
 				      			</tr>
 			      			</thead>
-			      			<?php
-			      				$vga = mysqli_query($koneksi,"select *from vga");			      				
+			      			<?php		      				
 								$i = 0;
 
 			      				if($vga == TRUE){
@@ -221,28 +263,64 @@
 						      				<td><?php echo $value ?></td>
 						      				<td><?php echo $nap ?></td>
 						      				<td><b><?php echo $vgascore ?></b></td>
+						      				<td></td>
 						      			</tr>
 
 						      			<?php
 									}
 								}
 			      			?>
-						</table>
 			      	</div>
-			      	<div class="modal-footer">
-			        	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			        	<button type="button" class="btn btn-primary">Save changes</button>
+			      	<div class="modal-footer">	
+			      			<form action="" method="POST">		      						        	
+				        		<tr>
+				      				<td></td>
+				      				<td><input class="form-control" type="text" name="vganame" placeholder="VGA Name" maxlength="100" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="gaming" placeholder="Gaming" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="graphics" placeholder="Graphic" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="computing" placeholder="Computing" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="ppw" placeholder="Performance Per Watt" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="value" placeholder="Value" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="nap" placeholder="Noise and Power" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="vgascore" placeholder="Score" maxlength="5" required></td>
+				      				<td><input class="btn btn-success" type="submit" name="vgaadd" value="Add"></td>
+				      			</tr>
+				      		</form>
+						</table>
 			      	</div>
 		    	</div>
 		  	</div>
-		</div>    
+		  	<?php
+			  	if(isset($_POST['vgaadd'])){
+
+				    $vganame = $_POST['vganame'];
+				    $gaming = $_POST['gaming'];
+				    $graphics = $_POST['graphics'];
+				    $computing = $_POST['computing'];
+				    $ppw = $_POST['ppw'];
+				    $value = $_POST['value'];
+				    $nap = $_POST['nap'];
+				    $vgascore = $_POST['vgascore'];
+
+				    $cpuadd = mysqli_query($koneksi,"insert into vga(vganame,gaming,graphics,computing,ppw,value,nap,vgascore) values ('$vganame','$gaming','$graphics','$computing','$ppw','$value','$nap','$vgascore')");
+
+				    echo "<meta http-equiv='refresh' content='0'>";						   
+			  	}
+			?>
+		</div>  
 
 		<!-- SSD Modal -->
 	    <div class="modal fade" id="ssdmodal" tabindex="-1" role="dialog" aria-hidden="true">
 		  	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 		    	<div class="modal-content">
 		      		<div class="modal-header">
-		        		<img src="img/ssd.png" style="max-width: 3%; and height: auto"><h5 class="modal-title" id="exampleModalLongTitle">&nbsp; Solid State Disk List</h5>
+
+		      			<?php 
+		      				$ssd = mysqli_query($koneksi,"select *from ssd");
+		      				$rssd = mysqli_num_rows($ssd);
+						?>		      			
+
+		        		<img src="img/ssd.png" style="max-width: 3%; and height: auto"><h5 class="modal-title" id="exampleModalLongTitle">&nbsp; Solid State Disk List (<?php echo $rssd; ?> Product)</h5></h5>
 		        		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 		          			<span aria-hidden="true">&times;</span>
 		        		</button>
@@ -252,16 +330,16 @@
 			        		<thead class="thead-light">
 				      			<tr>
 				      				<th>No</th>
-				      				<th width="30%">SSD Name</th>
+				      				<th width="20%">SSD Name</th>
 				      				<th>Read Performance</th>
 				      				<th>Write Performance</th>
 				      				<th>Real World Benchmarks</th>
 				      				<th>Benchmarks</th>
 				      				<th width="10%">Total Score</th>
+				      				<th></th>
 				      			</tr>
 			      			</thead>
-			      			<?php
-			      				$ssd = mysqli_query($koneksi,"select *from ssd");			      				
+			      			<?php		      				
 								$i = 0;
 
 			      				if($ssd == TRUE){
@@ -282,20 +360,46 @@
 						      				<td><?php echo $realwb ?></td>
 						      				<td><?php echo $bench ?></td>
 						      				<td><b><?php echo $ssdscore ?></b></td>
+						      				<td></td>
 						      			</tr>
 
 						      			<?php
 									}
 								}
 			      			?>
-						</table>
 			      	</div>
-			      	<div class="modal-footer">
-			        	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			        	<button type="button" class="btn btn-primary">Save changes</button>
+			      	<div class="modal-footer">	
+			      			<form action="" method="POST">		      						        	
+				        		<tr>
+				      				<td></td>
+				      				<td><input class="form-control" type="text" name="ssdname" placeholder="SSD Name" maxlength="100" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="readp" placeholder="Read Performance" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="writep" placeholder="Write Performance" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="realwb" placeholder="Real World Benchmarks" maxlength="5" required></td>
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="bench" placeholder="Benchmarks" maxlength="5" required></td>				      				
+				      				<td><input class="form-control" type="number" min="0" max="10" step="0.1" name="ssdscore" placeholder="Score" maxlength="5" required></td>
+				      				<td><input class="btn btn-success" type="submit" name="ssdadd" value="Add"></td>
+				      			</tr>
+				      		</form>
+						</table>
 			      	</div>
 		    	</div>
 		  	</div>
+		  	<?php
+			  	if(isset($_POST['ssdadd'])){
+
+				    $ssdname = $_POST['ssdname'];
+				    $readp = $_POST['readp'];
+				    $writep = $_POST['writep'];
+				    $realwb = $_POST['realwb'];
+				    $bench = $_POST['bench'];
+				    $ssdscore = $_POST['ssdscore'];
+
+				    $cpuadd = mysqli_query($koneksi,"insert into ssd(ssdname,readp,writep,realwb,bench,ssdscore) values ('$ssdname','$readp','$writep','$realwb','$bench','$ssdscore')");
+
+				    echo "<meta http-equiv='refresh' content='0'>";						   
+			  	}
+			?>
 		</div>  
 
 	</body>
